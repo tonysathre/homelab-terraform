5d1512f7440bb8540c5666ef09158cd8fd28af69
Tony Sathre <tony@ghostbit.org>
2022-04-07 22:30:50 -0500

    added domain controllers

---
+++ vsphere-vm/main.tf
@@ -0,0 +1,56 @@
+provider "vsphere" {
+  user           = var.vcenter_user
+  password       = var.vcenter_password
+  vsphere_server = var.vcenter_server
+
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+locals {
+  date = formatdate("YYYY-MM-DD hh:mm:ss ZZZ", timestamp())
+}
+
+module "domain-controllers" {
+  source               = "Terraform-VMWare-Modules/vm/vsphere"
+  version              = "3.4.1"
+  cpu_number           = var.num_cpus
+  num_cores_per_socket = var.num_cores_per_socket
+  ram_size             = var.memory
+  instances            = var.instances
+  dc                   = var.datacenter
+  vmrp                 = "${var.cluster}/Resources"
+  vmfolder             = var.folder
+  datastore            = var.datastore
+  vmtemp               = var.template
+  vmname               = var.vm_name_prefix
+  vmnameformat         = var.vm_name_format #To use three decimal with leading zero vmnames will be AdvancedVM001,AdvancedVM002
+  #domain            = "somedomain.com"
+  network = {
+    "VM Network" = var.ipv4_address # To use DHCP create Empty list ["",""]; You can also use a CIDR annotation;
+  }
+  ipv4submask  = var.ipv4_netmask
+  network_type = ["vmxnet3"]
+
+  scsi_type       = "pvscsi" // Other acceptable value "pvscsi"
+  scsi_controller = 0        // This will assign OS disk to controller 0
+  dns_server_list = var.dns_server_list
+  vmgateway       = var.ipv4_gateway
+  auto_logon      = var.autologon
+  #run_once         = ["command01", "command02"] // You can also run Powershell commands
+  orgname          = var.organization_name
+  workgroup        = var.workgroup
+  is_windows_image = true
+  firmware         = var.firmware
+  local_adminpass  = var.admin_password
+  force_power_off  = false
+  annotation       = "${var.annotation} ${local.date}"
+}
+
+output "vmnames" {
+  value = module.domain-controllers.VM
+}
+
+output "vmnameswip" {
+  value = module.domain-controllers.ip
+}
---
+++ vsphere-vm/variables.tf
@@ -0,0 +1,117 @@
+variable "instances" {
+  type = number
+}
+
+variable "vcenter_server" {
+  type = string
+}
+
+variable "vcenter_user" {
+  type      = string
+  sensitive = true
+}
+
+variable "vcenter_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "datacenter" {
+  type = string
+}
+
+variable "datastore" {
+  type = string
+}
+
+variable "cluster" {
+  type = string
+}
+
+variable "folder" {
+  type = string
+}
+
+variable "network" {
+  type = string
+}
+
+variable "template" {
+  type = string
+}
+
+variable "num_cpus" {
+  type = number
+}
+
+variable "num_cores_per_socket" {
+  type = number
+}
+
+variable "memory" {
+  type = number
+}
+
+variable "firmware" {
+  type = string
+}
+
+variable "guest_id" {
+  type = string
+}
+
+variable "vm_name_prefix" {
+  type = string
+}
+
+variable "vm_name_format" {
+  type = string
+}
+
+variable "hostname" {
+  type = string
+}
+
+variable "workgroup" {
+  type = string
+}
+
+variable "organization_name" {
+  type = string
+}
+
+variable "time_zone" {
+  type = number
+}
+
+variable "autologon" {
+  type = bool
+}
+
+variable "admin_password" {
+  type = string
+}
+
+variable "ipv4_address" {
+  type = list(any)
+}
+
+variable "ipv4_netmask" {
+  type = list(string)
+}
+
+variable "ipv4_gateway" {
+  type = string
+}
+
+variable "dns_server_list" {
+  type = list(any)
+}
+
+variable "dns_domain" {
+  type = string
+}
+
+variable "annotation" {
+  type = string
+}
